fb5417e8d8f7c8e67dde9e8552c5b6fe78d3c32a
samat <samhardy1927@gmail.com>
2022-03-29 12:23:00 +0300

    ABSTRACTtask

---
+++ src/OOP/lesson18Abstract/AbstractDomesticAnimals.java
@@ -0,0 +1,55 @@
+package OOP.lesson18Abstract;
+
+public abstract class AbstractDomesticAnimals {
+    private double weight;
+    private int age;
+    private String gender;
+    private String nickName;
+
+
+    public AbstractDomesticAnimals (double weight, int age, String gender, String nickName) {
+        this.weight = weight;
+        this.age = age;
+        this.gender = gender;
+        this.nickName = nickName;
+    }
+
+    public AbstractDomesticAnimals() {
+
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public abstract void voice();
+
+}
