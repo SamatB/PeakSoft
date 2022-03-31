60ded8b65cd4b7dace34b159712bd47757199be9
samat <samhardy1927@gmail.com>
2022-04-01 01:45:41 +0300

    lesson19

--- src/OOP/lesson17Polyphormism/Task17/Eagle.java
+++ src/OOP/lesson17Polyphormism/Task17/Eagle.java
@@ -1,28 +1,28 @@
-package OOP.lesson17Polyphormism.Task17;
-
-public class Eagle extends Animal{
-    public Eagle(String name) {
-        super(name);
-    }
-
-    public Eagle() {
-
-    }
-
-    @Override
-    public void action () {
-        System.out.println("Eagle hunts for turtle...");
-    }
-
-    public  void fly () {
-        System.out.println("Eagle hunts for turtle...");
-    }
-<<<<<<< HEAD
-
-    @Override
-    public String toString () {
-        return "Eagle is on hunting";
-    }
-=======
->>>>>>> origin/master
-}
+//package OOP.lesson17Polyphormism.Task17;
+//
+//public class Eagle extends Animal{
+//    public Eagle(String name) {
+//        super(name);
+//    }
+//
+//    public Eagle() {
+//
+//    }
+//
+//    @Override
+//    public void action () {
+//        System.out.println("Eagle hunts for turtle...");
+//    }
+//
+//    public  void fly () {
+//        System.out.println("Eagle hunts for turtle...");
+//    }
+//<<<<<<< HEAD
+//
+//    @Override
+//    public String toString () {
+//        return "Eagle is on hunting";
+//    }
+//=======
+//>>>>>>> origin/master
+//}
